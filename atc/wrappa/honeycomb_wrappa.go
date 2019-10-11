ec2edbf790d22d4c4af3f13bd852b426a7837700
Nat Bennett <nbennett@pivotal.io>
2019-10-11 09:46:25 -0700

    Enable emitting honeycomb events

---
+++ atc/wrappa/honeycomb_wrappa.go
@@ -0,0 +1,29 @@
+package wrappa
+
+import (
+	"github.com/honeycombio/libhoney-go"
+	"github.com/tedsuo/rata"
+)
+
+type HoneycombWrappa struct {
+	client *libhoney.Client
+}
+
+func NewHoneycombWrappa(client *libhoney.Client) Wrappa {
+	return HoneycombWrappa{
+		client: client,
+	}
+}
+
+func (wrappa HoneycombWrappa) Wrap(handlers rata.Handlers) rata.Handlers {
+	wrapped := rata.Handlers{}
+
+	for name, handler := range handlers {
+		wrapped[name] = HoneycombHandler{
+			Client:  wrappa.client,
+			Handler: handler,
+		}
+	}
+
+	return wrapped
+}
